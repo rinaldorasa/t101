1bc9e5f708a90b67a22200c9f34884af86c66997
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2021-01-25 18:41:02 +0100

    Add files via upload

--- viewdir.php
+++ viewdir.php
@@ -242,6 +242,24 @@ style="position:static; left:3em;top:4em;">
 
 </main> 
 
+<?php 
+  
+// Set the current working directory 
+$directory = getcwd()."/"; 
+  
+// Initialize filecount variavle 
+$filecount = 0; 
+  
+$files2 = glob( $directory ."*" ); 
+  
+if( $files2 ) { 
+    $filecount = count($files2); 
+} 
+  
+echo $filecount . " files "; 
+  
+?> 
+
 
 
 <blockquote style="margin-top:2rem; 
