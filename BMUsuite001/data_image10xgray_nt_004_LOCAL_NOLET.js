16d59980964ef4658a59d0e0f734c390d5b443ea
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2021-09-26 19:38:28 +0200

    Add files via upload

---
+++ BMUsuite001/data_image10xgray_nt_004_LOCAL_NOLET.js
@@ -0,0 +1,8 @@
+//
+// data_image10xgray_nt_004_LOCAL_NOLET.js
+//
+// prova
+
+
+
+data_image10xgray_nt_004 = " sono data_image10xgray_nt_004 ";
---
+++ BMUsuite001/unico_carta_inni_3_0_0.js
@@ -0,0 +1,612 @@
+/* **** **** **** **** 
+
+
+UNICO 3.0.0
+
+2021:09:26 19:15
+
+
+**** **** **** **** */
+
+
+
+// i CV 0 1 9
+// determinano il koeff
+//
+
+
+
+/////////////////////////////////////////////////////////////////////
+//                                                                 //
+// ex mini_n_t_xxx_test0.0.2_.js                                   //
+//                                                                 //
+//
+//
+//
+
+
+"use strict";
+
+//--1234567890123456
+let LET_______AVATAR;  // gog cdn visible
+let NOME_____COGNOME;  // text visible
+let LET________EMAIL;  // mailto visible
+let LET____NUM_TEL_C;  // gog cdn visible
+let LET____NUM_TEL_K;
+ 
+
+//////////////////////////////////////////////////
+// se si devono fare aggiustamenti              //
+// primo passaggio                              //
+// (con definizione delle variabili)            //
+//                                              //
+// nei passaggi successivi                      //
+//
+//
+
+let val_numero_KRIPTO = VAL__KRIPTO_TEST;
+
+
+let KRIPTO = "" ;     // inizializzazione
+
+ 
+let DEKRIPTO = "" ;   // inizializzazione  
+
+ 
+
+let contachiaro;
+let lunghezza_CHIARO;
+let car_CHIARO;
+let unicode_CHIARO;
+let unicode_KRIPTO;
+let car_KRIPTO;
+let lunghezza_KRIPTO;
+let unicode_DEKRIPTO;
+
+
+let OGGI; // unused ?
+let data_400_n_t_002; // unused ?
+
+
+let data_image_n_t_001;
+let data_image400_me_gog_dekripto;
+
+
+let data_image_n_t_002;                // unused ???
+let data_image400gray_nt_dekripto;     // unused ???
+
+
+
+let data_image410gray_nt_002_dekripto;
+
+
+let data_image10xgray_nt_003_dekripto;
+
+
+let data_image_410_nt_005_LOCAL_NET_dekripto;
+
+
+
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+/* variabili di collegamento tra UNICO e CARTA  */
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+let idSCRIPT0___0;  // quello principale 0 1 9
+
+let idSCRIPT1___0;  // quello more info
+
+let idSCRIPT2___0;  // quello more info
+
+
+
+
+
+
+//////////////////////////////////////////////////////////
+// the main program (HTML) check if you                  //
+// are a robot                                           //
+                                                         //
+                                                         //
+                                                         //
+let CHECKED_WHOU_ARE;                                    //
+                                                         //
+CHECKED_WHOU_ARE = I_AM_NOT_A_ROBOT;                     //
+CHECKED_WHOU_ARE = CHECKED_WHOU_ARE.toString(10);        //
+                                                         //
+                                                         //
+//////////////////////////////////////////////////////////
+//   se I_AM_NOT_A_ROBOT==="0" salta i CKD               //
+//                                                       //
+/////////////////////////////////////////////////////////////////////////////
+                                                                           //
+DEKRIPTO="";                                                               //
+lunghezza_KRIPTO=KRIPTO.length;                                            //
+for(contachiaro=0;contachiaro<lunghezza_KRIPTO;contachiaro++)              //
+{                                                                          //
+  unicode_DEKRIPTO=KRIPTO.charCodeAt(contachiaro)-val_numero_KRIPTO;       //
+  DEKRIPTO=DEKRIPTO+String.fromCharCode(unicode_DEKRIPTO);                 //
+}                                                                          //
+DEKRIPTO="";                                                               //
+                                                                           //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////
+// dati biografici dell'autore
+
+LET_______AVATAR = '<img style="width:2.5rem;height:2.5rem;border-radius:50%;" src="http://edoraandiamo.altervista.org/null/gog/download_avatar_rinaldo.png" title="" alt="" >';
+
+NOME_____COGNOME = "<span class='bg-light'> Rinaldo Rasa </span>" ;
+
+LET________EMAIL = "<a href='mailto:rinaldo.rasa@gmail.com' ><h6 class='badge bg-success' style='font-size:small!important;font-variant:small-caps;'>Email</h6></a>";
+
+
+// se non sei un robot vedi
+if(CHECKED_WHOU_ARE==="1"){
+
+LET____NUM_TEL_C = '<img style="width:auto;height:auto;border-radius:0%;" src="http://edoraandiamo.altervista.org/null/gog/download_telcasa.png" alt="" title=""  />';
+
+}
+else {  }
+
+
+
+//let data_image_n_t_001;
+//
+data_image_n_t_001 = LET____NUM_TEL_C;  // num tel fisso
+
+LET____NUM_TEL_C = data_image_n_t_001;  // 
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////
+//                                                  //
+// INFORMAZIONI GENERICHE                           //
+// informazioni con koefficente                     //
+//                                                  //
+//                                                  //
+/*--1234567890123456*/                              //
+////////////////////////////////////////////////////// 
+                                                    
+                                                    
+                                                    
+//////////////////////////////////////////////////////
+//                                                  //
+// koefficente con controllo I_AM_NOT_A_ROBOT       //
+//                                                  //
+//                                                  //
+//                                                  //
+// ME_GOG (google accounts)                         //
+// 400 = (4 x 10)*10 ()                             //
+// data_image400gray_me_gog.js                      //
+//                                                  //
+// nel caso si facciano test occorre                //
+// dare un valore di default                        //
+// alle variabili che si vogliono                   //
+// processare                                       //
+//                                                  //
+// se variabile non Ã¨ immagine non si              //
+// produce dekripto quindi silenziare               //
+// il trasferimento                                 //
+//                                                  //
+// VARIABILE DEFINITA IN HEAD                       //
+//let data_image400gray_me_gog;                     //
+//data_image400gray_me_gog="";                      //
+//
+//
+// se non sei un robot dekripta
+if(CHECKED_WHOU_ARE==="1"){
+KRIPTO = data_image400gray_me_gog ;
+DEKRIPTO="";
+lunghezza_KRIPTO=KRIPTO.length;
+for(contachiaro=0;contachiaro<lunghezza_KRIPTO;contachiaro++)
+{
+  unicode_DEKRIPTO=KRIPTO.charCodeAt(contachiaro)-val_numero_KRIPTO;
+  DEKRIPTO=DEKRIPTO+String.fromCharCode(unicode_DEKRIPTO);
+}
+ 
+//let data_image400_me_gog_dekripto;
+data_image400_me_gog_dekripto = DEKRIPTO ;
+
+} // if CHECKED_WHOU_ARE
+
+
+DEKRIPTO="";
+// 
+//////////////////////////////////////////// 
+ 
+
+
+
+ 
+//////////////////////////////////////////////////
+// (immagine png)
+// NT_XXX ATTUALMENTE UNUSED 
+// 400 = (4 x 10)*10 ()
+// 
+/*
+KRIPTO = data_image400gray_nt_000 ;
+DEKRIPTO="";
+lunghezza_KRIPTO=KRIPTO.length;
+for(contachiaro=0;contachiaro<lunghezza_KRIPTO;contachiaro++)
+{
+  unicode_DEKRIPTO=KRIPTO.charCodeAt(contachiaro)-val_numero_KRIPTO;
+  DEKRIPTO=DEKRIPTO+String.fromCharCode(unicode_DEKRIPTO);
+}
+
+DEKRIPTO=""; 
+*/ 
+ 
+if(CHECKED_WHOU_ARE==="0"){
+ data_image400gray_nt_dekripto = "<span>&#9742</span>"; // phone
+}else
+     {
+ data_image400gray_nt_dekripto =  '<img src="http://edoraandiamo.altervista.org/null/gog/download_icophone.png" alt="" title="" >'; 
+     
+ 
+     }
+
+// 
+//////////////////////////////////////////// 
+
+
+
+
+//////////////////////////////////////////////////////
+//                                                  //
+// koefficente con controllo I_AM_NOT_A_ROBOT       //
+// data_image410gray_nt_002.js                      //
+
+// se non sei un robot dekripta
+if(CHECKED_WHOU_ARE==="1"){
+
+/*      qui la assegnazione valore per KRIPTO      */
+/*                                                 */
+/*                                                 */
+
+KRIPTO = data_image410gray_nt_002 ;
+
+DEKRIPTO="";
+lunghezza_KRIPTO=KRIPTO.length;
+for(contachiaro=0;contachiaro<lunghezza_KRIPTO;contachiaro++)
+{
+  unicode_DEKRIPTO=KRIPTO.charCodeAt(contachiaro)-val_numero_KRIPTO;
+  DEKRIPTO=DEKRIPTO+String.fromCharCode(unicode_DEKRIPTO);
+}
+ 
+//let data_image400_me_gog_dekripto;
+data_image410gray_nt_002_dekripto = DEKRIPTO ;
+
+} // if CHECKED_WHOU_ARE
+
+
+DEKRIPTO="";
+// 
+////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////
+//                                                  //
+// koefficente con controllo I_AM_NOT_A_ROBOT       //
+// data_image10xgray_nt_003.js
+//
+// ten wind half nok (style)
+// ^^^^^^^^^^^^^^^^^^^^^^^^^
+// ^^^^^^^^^^^^^^^^^^^^^^^^^
+// ^^^^^^^^^^^^^^^^^^^^^^^^^
+//
+// http://edoraandiamo.altervista.org/null/gog/magog/data_image10xgray_nt_003.js
+
+// se non sei un robot dekripta
+if(CHECKED_WHOU_ARE==="9"){
+/*                                                 */
+/*        assegnazione valore a KRIPTO             */
+
+KRIPTO = data_image10xgray_nt_003 ;
+
+
+DEKRIPTO="";
+lunghezza_KRIPTO=KRIPTO.length;
+for(contachiaro=0;contachiaro<lunghezza_KRIPTO;contachiaro++)
+{
+  unicode_DEKRIPTO=KRIPTO.charCodeAt(contachiaro)-val_numero_KRIPTO;
+  DEKRIPTO=DEKRIPTO+String.fromCharCode(unicode_DEKRIPTO);
+}
+ 
+//let data_image400_me_gog_dekripto;
+data_image10xgray_nt_003_dekripto = DEKRIPTO ;
+
+} // if CHECKED_WHOU_ARE
+
+
+DEKRIPTO="";
+// 
+////////////////////////////////////////////
+
+
+
+/* **** **** **** **** **** **** **** **** ****  */
+// 
+// il KD successivi al data_image10xgray_nt_003_dekripto
+// vanno decriptati qui di seguito e successivamente
+// smistati nelle variabili let idSCRIPTx___x
+// -----------------------------^^^^^^^^^---^
+// 
+/* **** **** **** **** **** **** **** **** ****  */
+
+
+
+
+
+////////////////////////////////////////////////////////
+////                                                  //
+//// koefficente con controllo I_AM_NOT_A_ROBOT       //
+//// data_image_410_nt_005_LOCAL_NET                  //
+//
+//// se non sei un robot dekripta
+//if(CHECKED_WHOU_ARE==="1"){
+//
+////*      qui la assegnazione valore per KRIPTO      */
+////*                                                 */
+////*                                                 */
+//
+//KRIPTO = data_image_410_nt_005_LOCAL_NET ;
+//
+//DEKRIPTO="";
+//lunghezza_KRIPTO=KRIPTO.length;
+//for(contachiaro=0;contachiaro<lunghezza_KRIPTO;contachiaro++)
+//{
+//  unicode_DEKRIPTO=KRIPTO.charCodeAt(contachiaro)-val_numero_KRIPTO;
+//  DEKRIPTO=DEKRIPTO+String.fromCharCode(unicode_DEKRIPTO);
+//}
+// 
+//// data_image_410_nt_005_LOCAL_NET_dekripto
+//data_image_410_nt_005_LOCAL_NET_dekripto = DEKRIPTO ;
+//
+//
+//} // if CHECKED_WHOU_ARE
+//
+//
+//DEKRIPTO="";
+//// 
+//////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* """"""""""""""""""""""""""""""""""""""""""""""""
+*  """"""""""""""""""""""""""""""""""""""""""""""""
+*  gli slot idSCRIPTx___x sono disponibili       ""
+*  e appaiono nella finestra degli items         ""
+*  con numerazione da 0 (min) a 9 (max)          ""
+*  naturalmente questi slot disponibili devono   ""
+*  essere predisposti nel CORE incrementando     ""
+*  appositamente il programma principale CORE    ""
+*                                                ""
+*                                                ""
+*                                                ""
+*                                                ""
+*/                                               ""
+//            ""                                 ""
+//            ""                                 ""
+//            ""                                 ""
+//            ""                                 ""
+//            \/                                 ""
+//												 ""
+/* **********************************************""
+                                                 ""
+ qui di seguito utilizzo dei dati messi          ""
+ in chiaro sia che essi siano immagini png 0     ""
+ oppure CV 1 9                                   ""
+												 ""
+************************************************ */
+/////////////////////////////////////////////////// 
+
+
+
+ idSCRIPT0___0 = 
+ LET_______AVATAR + 
+ NOME_____COGNOME +
+ LET________EMAIL +
+ LET____NUM_TEL_C +
+
+
+
+
+'<br>'+'*==>'+OGGI+'<==*'+
+
+//
+'<br>'+'*==>'+data_image400_me_gog_dekripto+'<==*'+
+
+//
+'<br>'+'*==>'+data_image400gray_nt_dekripto+'<==*'+
+
+
+//
+'<br>'+'<div id="410a" class="rounded" style="border:solid 0.125rem rgba('+reRGB+','+grRGB+','+blRGB+',1);overflow:auto;">*==>'+data_image410gray_nt_002_dekripto+'<==*</div>'+
+
+
+//
+'<br>'+'<div id="410b" class="rounded" style="border:solid 0.125rem rgba('+reRGB+','+grRGB+','+blRGB+',1);overflow:auto;">*==>'+data_image10xgray_nt_003_dekripto+'<==*</div>'+
+
+
+//
+'<br>'+'*<span style="background-color:rgba('+reRGB+','+grRGB+','+blRGB+',1);font-weight:normal;font-size:1rem;font-variant:small-caps;">CHECKED_WHOU_ARE==></span><span class="invisible">'+CHECKED_WHOU_ARE+'</span><span style="background-color:rgba('+reRGB+','+grRGB+','+blRGB+',1);font-weight:normal;font-size:1rem;font-variant:small-caps;"><==</span>*'+
+
+
+'<br>'+'<span style="opacity:0.25;background-color:rgba('+reRGB+','+grRGB+','+blRGB+',1);"> Questa è una istanza del programma al 2021:09:26:11:19 </span>'+'<br>'+
+
+'<span class="bg-danger text-light fs-6">sistemare le variabili in head del database per una centralizzazione</span>'+'<br>'+
+
+// 
+//////////////////////////////////////////// 
+
+
+
+
+'<br><span style="background-color:rgba('+reRGB+','+grRGB+','+blRGB+',1);color:rgba('+reRGB*0.5+','+grRGB*0.5+','+blRGB*0.5+',1);font-weight: normal;letter-spacing:0.125rem;">*fine del documento*</span>'+'<br>'+'<br>'+'<br>'+'<br>'+
+
+'';
+
+
+
+
+
+// altri slot variabili
+// se necessario
+/*
+let idSCRIPT0___1;
+let idSCRIPT0___2;
+let idSCRIPT0___3;
+let idSCRIPT0___4;
+let idSCRIPT0___5;
+let idSCRIPT0___6;
+let idSCRIPT0___7;
+let idSCRIPT0___8;
+let idSCRIPT0___9;
+*/
+/*
+let idSCRIPTx___1;
+let idSCRIPTx___2;
+let idSCRIPTx___3;
+let idSCRIPTx___4;
+let idSCRIPTx___5;
+let idSCRIPTx___6;
+let idSCRIPTx___7;
+let idSCRIPTx___8;
+let idSCRIPTx___9;
+*/
+
+
+/* ----* ----* ----* ----* ----*  */
+
+
+
+
+
+
+
+////////////////////////////////////////////
+// secondo item                           //
+//                                        //
+// 20210926 19:17                         //
+//                                        //
+//                                        //
+//                                        //
+//                                        //
+//                                        //
+//                                        //
+////////////////////////////////////////////
+
+
+let LETCOMMENTOTEST;
+
+LETCOMMENTOTEST = "";
+
+
+
+if(CHECKED_WHOU_ARE==="1"){
+
+KRIPTO = data_image410grey_nt_005 ;
+
+DEKRIPTO="";
+lunghezza_KRIPTO=KRIPTO.length;
+
+
+for(contachiaro=0;contachiaro<lunghezza_KRIPTO;contachiaro++)
+{
+  unicode_DEKRIPTO=KRIPTO.charCodeAt(contachiaro)-val_numero_KRIPTO;
+  DEKRIPTO=DEKRIPTO+String.fromCharCode(unicode_DEKRIPTO);
+}
+
+}
+
+//////////////////////////////////////
+// 
+idSCRIPT1___0 = "" + DEKRIPTO  + "";
+
+DEKRIPTO = "" ;
+
+
+if(CHECKED_WHOU_ARE==="1"){
+
+KRIPTO = data_image410grey_nt_006 ;
+
+DEKRIPTO="";
+lunghezza_KRIPTO=KRIPTO.length;
+
+
+for(contachiaro=0;contachiaro<lunghezza_KRIPTO;contachiaro++)
+{
+  unicode_DEKRIPTO=KRIPTO.charCodeAt(contachiaro)-val_numero_KRIPTO;
+  DEKRIPTO=DEKRIPTO+String.fromCharCode(unicode_DEKRIPTO);
+}
+
+}
+
+//////////////////////////////////////
+// 
+idSCRIPT1___0 = "<style>img{width:50%;}</style>" + idSCRIPT1___0 + "" + DEKRIPTO  + "";
+
+DEKRIPTO = "" ;  // si inizializza vuoto il dekripto
+
+
+
+
+
+
+
+
+//// valore che si
+//// trasferisce in CARTAx
+//// slot 1 
+//////////////////////////////////////
+
+
+
+
+
+
+
+////////////////////////////////////////////
+// terzo item                             //
+//                                        //
+// 20210925 09:28                         //
+//                                        //
+//                                        //
+//                                        //
+//                                        //
+//                                        //
+//                                        //
+////////////////////////////////////////////
+
+
+
+// let idSCRIPT2___0;
+
+idSCRIPT2___0 = " sono il terzo elemento!!! ";
+
+
+
+
+
