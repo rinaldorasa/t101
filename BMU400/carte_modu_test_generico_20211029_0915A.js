5aad659dbf044b0bdcb4cf3f796004113ea2ecc8
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2021-11-04 19:27:55 +0100

    Add files via upload

---
+++ BMU400/carte_modu_test_generico_20211029_0915A.js
@@ -0,0 +1,205 @@
+/*    testitem 1    */
+
+/*
+* valori globali:
+* ================
+* 1234567890123456
+* ||||||||||||||||
+* VAL__KRIPTO_TEST
+* I_AM_NOT_A_ROBOT
+* 1234567890123456
+* ||||||||||||||||
+* idSCRIPT0___0
+* idSCRIPT1___0
+* idSCRIPT2___0
+* idSCRIPT3___0
+* idSCRIPT4___0
+* idSCRIPT5___0
+* idSCRIPT6___0
+* idSCRIPT7___0
+* idSCRIPT8___0
+* idSCRIPT9___0
+* 1234567890123456
+* modu
+*
+*
+*
+*
+*
+*
+*
+*
+
+*/
+
+
+modu = "";
+
+
+    DEKRIPTO_test="";
+//  KRIPTO_test = "{o}~y*ò*x*~o}~";
+//  <div style="background-color:orange;color:indigo">eccomi</div>
+//    
+
+KRIPTO_test = "Fns*}~voG,lkmuq|yxn7myvy|Dy|kxqoEmyvy|Dsxnsqy,HommywsF9nsH";
+
+
+    lunghezza_KRIPTO_test=KRIPTO_test.length
+    
+    val_numero_KRIPTO_test=parseInt(VAL__KRIPTO_TEST);  // 10
+    
+    for(contachiaro_test=0;contachiaro_test<lunghezza_KRIPTO_test;contachiaro_test++)
+    {
+      unicode_DEKRIPTO_test=KRIPTO_test.charCodeAt(contachiaro_test)-val_numero_KRIPTO_test;
+      DEKRIPTO_test=DEKRIPTO_test+String.fromCharCode(unicode_DEKRIPTO_test);
+    }
+
+    testing1 = "DEKRIPTO_test==>"+DEKRIPTO_test;
+
+
+    DEKRIPTO_test="";
+
+KRIPTO_test = "{o}~y*ò*sv*~o}~*xwo|y*no";
+
+    lunghezza_KRIPTO_test=KRIPTO_test.length
+    
+    val_numero_KRIPTO_test=parseInt(VAL__KRIPTO_TEST);  // 10
+    
+    for(contachiaro_test=0;contachiaro_test<lunghezza_KRIPTO_test;contachiaro_test++)
+    {
+      unicode_DEKRIPTO_test=KRIPTO_test.charCodeAt(contachiaro_test)-val_numero_KRIPTO_test;
+      DEKRIPTO_test=DEKRIPTO_test+String.fromCharCode(unicode_DEKRIPTO_test);
+    }
+
+    testing2 = "DEKRIPTO_test==>"+DEKRIPTO_test;
+
+
+
+
+
+modu = testing1 + testing2 ;
+
+idSCRIPT1___0 = modu;
+
+
+
+
+
+
+
+
+
+
+/* ****************************************
+reference
+**************************************** */
+/*
+    <script>
+    let CHIARO_test;   
+    //----(+)----
+    //----\_/----
+    let val_numero_KRIPTO_test;  
+    //----(+)----
+    //----\_/----
+    let KRIPTO_test;  
+    //----(+)----
+    //----\_/----
+    let DEKRIPTO_test;   
+    CHIARO_test = "";   
+    let contachiaro_test;
+    let lunghezza_CHIARO_test;
+    let car_CHIARO_test;
+    let unicode_CHIARO_test;
+    let unicode_KRIPTO_test;
+    let car_KRIPTO_test;
+    let lunghezza_KRIPTO_test;
+    let unicode_DEKRIPTO_test;
+    </script>
+    <ARTICLE>
+    <div id="b64ckd" class="bg-dark" style="font-variant:small-caps;text-align:center;">
+    <blockquote style="font-variant:normal;"><span></span></blockquote>
+    <code style="DISPLAY:BLOCK;background-color:lightgreen;FONT-WEIGHT:BOLD;">chiarokriptodekripto.</code>
+    </div>
+    <input id="val_CHIARO_test" type="text" value=""><br>
+    <input id="val_KRIPTO_test" type="text" value="0"><br>
+    <button onclick="codice()" style="border-radius:0.25rem;background-color:lightgreen;border:solid 0.25rem indigo;">Entra</button>
+    <div id="test_test" class="mx-2" style="FONT-SIZE:1.150REM;inline-block;">?</div>
+    <script>
+    function codice(){
+    val_numero_KRIPTO_test=document.getElementById("val_KRIPTO_test").value;
+    val_numero_KRIPTO_test=parseInt(val_numero_KRIPTO_test);
+    CHIARO_test = document.getElementById("val_CHIARO_test").value;
+    lunghezza_CHIARO_test=CHIARO_test.length;
+    KRIPTO_test=""; car_CHIARO_test="";
+    for(contachiaro_test=0;contachiaro_test<lunghezza_CHIARO_test;contachiaro_test++)
+    {
+      unicode_CHIARO_test=CHIARO_test.charCodeAt(contachiaro_test);
+      unicode_KRIPTO_test=unicode_CHIARO_test+val_numero_KRIPTO_test;
+      car_CHIARO_test =  CHIARO_test.charAt(contachiaro_test);
+      car_KRIPTO_test =  String.fromCharCode(unicode_KRIPTO_test);
+      
+      KRIPTO_test=KRIPTO_test+car_KRIPTO_test;
+     
+    }
+    DEKRIPTO_test="";
+    lunghezza_KRIPTO_test=KRIPTO_test.length
+    for(contachiaro_test=0;contachiaro_test<lunghezza_KRIPTO_test;contachiaro_test++)
+    {
+      unicode_DEKRIPTO_test=KRIPTO_test.charCodeAt(contachiaro_test)-val_numero_KRIPTO_test;
+      DEKRIPTO_test=DEKRIPTO_test+String.fromCharCode(unicode_DEKRIPTO_test);
+    }
+    document.getElementById("test_test").innerHTML=
+    '   CHIARO            ==><span style="background-color:pink;OVERFLOW:AUTO;">'                          +CHIARO_test   +'</span><=='  +typeof(CHIARO_test)            +' <br>'+
+    '   val_numero_KRIPTO ==><span style="background-color:pink;OVERFLOW:AUTO;">'               +val_numero_KRIPTO_test   +'</span><=='  +typeof(val_numero_KRIPTO_test) +' <br>'+
+    '<article class="my-3">'+
+    '<button type="button" class="btn btn-sm btn-success bg-success" style="margin-top:0.25rem;font-weight:bold;font-family:sans-serif;font-variant:small-caps;" onclick="copyToClipboard(`#p1_test`)">Copia KRIPTO negli appunti</button>'+
+    ' <div class="py-3 bg-light border borde-1 border-danger rounded" STYLE="OVERFLOW:AUTO">'+
+    '   KRIPTO            ==><BR><span id="p1_test" style="background-color:pink;OVERFLOW:AUTO;">' + KRIPTO_test + '</span><BR><=='  +typeof(KRIPTO_test)            +' <br>'+
+    ' </div>'+
+    '</article>'+
+    '   DEKRIPTO_test          ==><span style="background-color:pink;OVERFLOW:AUTO;">'                        +DEKRIPTO_test   +'</span><=='  +typeof(DEKRIPTO_test)          +' <br>'+
+    '   contachiaro_test       ==><span style="background-color:pink;OVERFLOW:AUTO;">'                     +contachiaro_test   +'</span><=='  +typeof(contachiaro_test)       +' <br>'+
+    '   lunghezza_CHIARO_test  ==><span style="background-color:pink;OVERFLOW:AUTO;">'                +lunghezza_CHIARO_test   +'</span><=='  +typeof(lunghezza_CHIARO_test)  +' <br>'+
+    '   car_CHIARO_test        ==><span style="background-color:pink;OVERFLOW:AUTO;">'                      +car_CHIARO_test   +'</span><=='  +typeof(car_CHIARO_test)        +' <br>'+
+    '   unicode_CHIARO_test    ==><span style="background-color:pink;OVERFLOW:AUTO;">'                  +unicode_CHIARO_test   +'</span><=='  +typeof(unicode_CHIARO_test)    +' <br>'+
+    '   unicode_KRIPTO_test    ==><span style="background-color:pink;OVERFLOW:AUTO;">'                  +unicode_KRIPTO_test   +'</span><=='  +typeof(unicode_KRIPTO_test)    +' <br>'+
+    '   car_KRIPTO_test        ==><span style="background-color:pink;OVERFLOW:AUTO;">'                      +car_KRIPTO_test   +'</span><=='  +typeof(car_KRIPTO_test)        +' <br>'+
+    '   lunghezza_KRIPTO_test  ==><span style="background-color:pink;OVERFLOW:AUTO;">'                +lunghezza_KRIPTO_test   +'</span><=='  +typeof(lunghezza_KRIPTO_test)  +' <br>'+
+    '   unicode_DEKRIPTO_test  ==><span style="background-color:pink;OVERFLOW:AUTO;">'                +unicode_DEKRIPTO_test   +'</span><=='  +typeof(unicode_DEKRIPTO_test)  +' <br>'+
+    '   DEKRIPTO_test          ==><span style="background-color:pink;OVERFLOW:AUTO;">'                        +DEKRIPTO_test   +'</span><=='  +typeof(DEKRIPTO_test)          +' <br>'+
+    ' <br>'+
+    '';
+    } // codice()
+    //******************************************
+    //
+    // copyToClipboard
+    //******************************************
+    let element_test;
+    function copyToClipboard(element_test) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element_test).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+    }
+    </script>
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
