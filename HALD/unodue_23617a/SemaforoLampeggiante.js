2d051cf69a908731b5d3c939557fee00dd4f6341
Rinaldo Rasa <55927930+rinaldorasa@users.noreply.github.com>
2023-06-18 08:36:35 +0200

    Add files via upload

---
+++ HALD/unodue_23617a/SemaforoLampeggiante.js
@@ -0,0 +1,13 @@
+document.getElementById("Semaforo_Lampeggiante_Per_CapoPagina__").innerHTML=
+`
+<style>
+#CHIUDI_CapoPagina__{visibility:hidden;}
+#CONTENITORE_COPY_SI_NO__{visibility:hidden;}
+
+
+#QUESTION_MARK_POPUP__{visibility:hidden;}
+#ALT_QUESTION_MARK_POPUP__{visibility:hidden;}
+
+
+</style>
+`;
